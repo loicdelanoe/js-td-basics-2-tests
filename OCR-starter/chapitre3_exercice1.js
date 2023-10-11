bb6d26f3a9a2184f1d4140adcbbed19e04862421
Loïc Delanoë <loic.del4127@gmail.com>
2023-10-11 12:28:59 +0200

    Finished Chap3 Ex1

--- OCR-starter/chapitre3_exercice1.js
+++ OCR-starter/chapitre3_exercice1.js
@@ -8,3 +8,33 @@ affiche dasn la console "Erreur : jour non reconnu !"
 - Testez si la variable qui conntienyt le nom du jour n'ets pas vide et,
 dans ce cas, affichez dans la console le message "Demain, nous serons …"
 */
+const getDay = new Date().getDay();
+const dayNames = ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi"];
+
+const currentDay = dayNames[getDay];
+
+switch (currentDay) {
+    case "Dimanche":
+        console.log("Demain nous serons Lundi");
+        break;
+    case "Lundi":
+        console.log("Demain nous serons Mardi");
+        break;
+    case "Mardi":
+        console.log("Demain nous serons Mercredi");
+        break;
+    case "Mercredi":
+        console.log("Demain nous serons Jeudi");
+        break;
+    case "Jeudi":
+        console.log("Demain nous serons Vendredi");
+        break;
+    case "Vendredi":
+        console.log("Demain nous serons Samedi");
+        break;
+    case "Samedi":
+        console.log("Demain nous serons Dimanche");
+        break;
+    default:
+        console.log("Erreur : jour non reconnu !")
+}
