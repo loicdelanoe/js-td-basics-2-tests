d6d1f269162e067278e3b0695fd92173460f355b
Loïc Delanoë <loic.del4127@gmail.com>
2023-10-13 17:20:46 +0200

    Finished Script 14

--- starter/script014.js
+++ starter/script014.js
@@ -12,6 +12,9 @@
 2. Dans le bloc, affichez "Je suis dans un bloc d'instructions soumis à une condition qui est toujours vraie, je vais donc toujours m'afficher"
 */
 
+// if (true) {
+//     console.log("Je suis dans un bloc d'instructions soumis à une condition qui est toujours vraie, je vais donc toujours m'afficher");
+// }
 
 
 /* Et l'inverse :
@@ -19,6 +22,9 @@
 2. Dans le bloc, affichez "Bon, je cause toujours, mais vu que je suis dans un bloc d'instructions soumis à une condition qui est toujours fausse, ce que je dis ne va jamais s'afficher"
 */
 
+// if (false) {
+//     console.log("Bon, je cause toujours, mais vu que je suis dans un bloc d'instructions soumis à une condition qui est toujours fausse, ce que je dis ne va jamais s'afficher");
+// }
 
 
 /*
@@ -26,12 +32,32 @@
 2. Si ce nombre est positif (et seulement dans ce cas), affichez "… est positif"
 */
 
+// const nombre = Number(prompt("Entrez un nombre :"));
+
+// if (nombre > 0) {
+//    console.log(`${nombre} est positif`);
+// } else {
+//    console.log(`${nombre} est négatif`);
+// }
+
 
 
 /*
 Ecrivez un programme qui dit "Bonjour" à l'utilisateur et n'accepte de lui répondre que si celui-ci lui dit "Bonjour" à son tour. Dans ce cas, le programme lui répond "Toi, tu es poli, tu me dis bonjour, je veux bien te parler… Comment t'appelles-tu ?". Il enregistre alors l'entrée dans une variable nom et, seulement si l'utilisateur a bien entré quelque chose dans le champ (il prend la peine de vérifier avant), il lui répond (dans la console), "Je t'aime bien, …".
 */
 
+// const answer = prompt("Bonjour");
+//
+// if (answer.toLowerCase() === "bonjour") {
+//     const nom = prompt("Toi, tu es poli, tu me dis bonjour, je veux bien te parler… Comment t'appelles-tu ?");
+//     if (nom) {
+//         console.log(`Je t'aime bien, ${nom}`);
+//     } else {
+//         console.log("Tu ne m'as pas dit ton nom !");
+//     }
+// } else {
+//     console.log("Tu ne m'as pas dit bonjour !");
+// }
 
 
 // 2. Exprimer une alternative : if / else
@@ -41,7 +67,14 @@ Ecrivez un programme qui dit "Bonjour" à l'utilisateur et n'accepte de lui rép
 2. Si ce nombre est positif, affichez "… est positif", sinon, affichez "… est négatif ou nul"
 */
 
-
+// let nombre = prompt("Entrez un nombre :");
+// nombre = parseInt(nombre);
+//
+// if (nombre > 0) {
+//     console.log(`${nombre} est positif`);
+// } else {
+//     console.log(`${nombre} est négatif`);
+// }
 
 /* 
 1. Initialisez la variable firstName à  John et enregistrez son état civil ('single') dans une variable civilStatus
@@ -49,7 +82,14 @@ Ecrivez un programme qui dit "Bonjour" à l'utilisateur et n'accepte de lui rép
 3. Changez l'état civil de John à ('married') et retestez
 */
 
-
+// const firstName = 'John';
+// const civilStatus = 'single';
+//
+// if (civilStatus === 'married') {
+//     console.log(`${firstName} est marié !`);
+// } else {
+//     console.log(`${firstName} va, on l'espère, bientôt se marier :)`);
+// }
 
 /* 
 1. Créez à présent une variable isMarried pour enregistrer l'état civil de John et, en imaginant qu'il est marié, initialisez-la à true
